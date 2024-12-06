6f39f74c91e7143373580a87effa24af4748b2dc
Emelly Freitas <emedoim333@gmail.com>
2024-12-06 15:26:40 -0300

    teste

--- src/components/tableFormDiversos.tsx
+++ src/components/tableFormDiversos.tsx
@@ -72,6 +72,8 @@ const PageComDesdobramentos: React.FC<PageComDesdobramentosProps> = ({
         const hoje = new Date();
         const dataVencimentoConvertida = new Date(dataVencimento);
 
+        console.log("teste");
+
         if (dataVencimentoConvertida < hoje) {
             setErrorMessage("A data de vencimento não pode ser anterior à data atual.");
             return;
